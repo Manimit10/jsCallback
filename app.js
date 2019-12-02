644b422be3b63a58ee235504c90970488f02968a
Mani <m.tahriri.z@gmail.com>
2019-12-02 15:15:27 +0100

    setTimeout() will add to first function,, so it will appear while after second function

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 // Create two function and invoke them one after another
-function a(){
-    console.log("This is function A");
+// function a(){
+//     console.log("This is function A");
+//   }
+  
+//   function b(){
+//     console.log("This is function B");
+//   }
+  
+//   a();
+//   b();
+//********************************* */
+  //create one normal funcion and one with setTimeout
+  function a(){
+    // create waiting time for function A
+    setTimeout( function(){
+      console.log("This is function A");
+    }, 2000 );
   }
   
   function b(){
@@ -8,4 +23,6 @@ function a(){
   }
   
   a();
-  b();
+  b();//function b() will execute first then with a delay function a() will start
+
+  //**************************** */
