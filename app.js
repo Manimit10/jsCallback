9c7e55c9b82a687e38b73a73ad12a4e2b85171d6
Mani <m.tahriri.z@gmail.com>
2019-12-02 15:18:08 +0100

    callback is added  to first function,, so it will execute first, no matter how long we should wait

--- app.js
+++ app.js
@@ -11,18 +11,33 @@
 //   b();
 //********************************* */
   //create one normal funcion and one with setTimeout
-  function a(){
-    // create waiting time for function A
+//   function a(){
+//     // create waiting time for function A
+//     setTimeout( function(){
+//       console.log("This is function A");
+//     }, 2000 );
+//   }
+  
+//   function b(){
+//     console.log("This is function B");
+//   }
+  
+//   a();
+//   b();//function b() will execute first then with a delay function a() will start
+
+  //**************************** */
+ //This Example will run function one first using callback
+ function a(callback){
     setTimeout( function(){
       console.log("This is function A");
+      callback();
     }, 2000 );
   }
   
   function b(){
     console.log("This is function B");
   }
-  
-  a();
-  b();//function b() will execute first then with a delay function a() will start
 
-  //**************************** */
+  a(b);
+
+  //****************************** */
