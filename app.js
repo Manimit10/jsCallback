a86785bb6e7c3f3e7e280d8e7a73cfcbc2339180
Mani <m.tahriri.z@gmail.com>
2019-12-02 15:20:59 +0100

    how to add argument and callback to a function

--- app.js
+++ app.js
@@ -27,17 +27,28 @@
 
   //**************************** */
  //This Example will run function one first using callback
- function a(callback){
-    setTimeout( function(){
-      console.log("This is function A");
-      callback();
-    }, 2000 );
-  }
+//  function a(callback){
+//     setTimeout( function(){
+//       console.log("This is function A");
+//       callback();
+//     }, 2000 );
+//   }
   
-  function b(){
-    console.log("This is function B");
-  }
+//   function b(){
+//     console.log("This is function B");
+//   }
 
-  a(b);
-
+//   a(b);
   //****************************** */
+    // This example show how to add argument and callback to a function
+  function StartEngine(engine, callback) {
+    setTimeout( function(){
+      console.log(`Starting the ${engine} now.`);
+      callback();
+    }, 2000);}
+    
+  function setGear(){
+    setTimeout( function(){
+    console.log('go to first gear');
+  },1000 );}
+  StartEngine('BMW', setGear);
