1aef1a13af65fba67765690e7276a4c758585e38
Mani <m.tahriri.z@gmail.com>
2019-12-02 15:13:12 +0100

    create two function and call them one after another

---
+++ app.js
@@ -0,0 +1,11 @@
+// Create two function and invoke them one after another
+function a(){
+    console.log("This is function A");
+  }
+  
+  function b(){
+    console.log("This is function B");
+  }
+  
+  a();
+  b();
